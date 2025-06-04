d233bcc5d711af02502418a54c0ff97676a3ee80
Yuta Sakurai <sakurai.yuta@gmail.com>
2025-06-04 15:45:59 +0900

    Use official fennel-ls repository

--- installer/install-fennel-ls.sh
+++ installer/install-fennel-ls.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-# FIXME: official repository is not available for Windows
-# git clone https://git.sr.ht/~xerool/fennel-ls src
-git clone https://git.sr.ht/~y_sakurai/fennel-ls src
+# Clone fennel-ls from the official repository
+git clone https://git.sr.ht/~xerool/fennel-ls src
 cd src/
 make
 cp fennel-ls ../
